9a8ef858e9f2c3db66039a06b577e44ca4ff13ab
SmurfNiZed <zipacaldo@up.edu.ph>
2024-02-20 15:28:20 +0800

    fix: deployment to hosting

--- transitrack_web/lib/components/map_related/map.dart
+++ transitrack_web/lib/components/map_related/map.dart
@@ -1,11 +1,12 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 
 import '../../config/keys.dart';
 
 class MapWidget extends StatefulWidget {
   final bool isHover;
-  const MapWidget({super.key, required this.isHover});
+  const MapWidget({Key? key, required this.isHover}) : super(key: key);
 
   @override
   State<MapWidget> createState() => _MapWidgetState();
@@ -13,9 +14,60 @@ class MapWidget extends StatefulWidget {
 
 class _MapWidgetState extends State<MapWidget> {
   late MapboxMapController _mapController;
+  List<Circle> circles = [];
 
   void _onMapCreated(MapboxMapController controller) {
     _mapController = controller;
+  }
+
+  Future<void> _getCurrentLocation() async {
+    await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.bestForNavigation)
+        .then(
+          (position) => {
+            _updateCircle(LatLng(position.latitude, position.longitude))
+          }
+        );
+  }
+
+  void _updateCircle(LatLng latLng) {
+
+    bool exists = false;
+
+    for (var circle in circles) {
+      if (circle.options.circleColor == '#3366FF') {
+        exists = true;
+        break;
+      }
+    }
+
+    if (exists) {
+      Circle deviceCircle = circles.where((circle) => circle.options.circleColor == '#3366FF').first;
+      _mapController.updateCircle(deviceCircle, CircleOptions(geometry: latLng));
+      circles.removeWhere((circle) => circle.options.circleColor == '#3366FF');
+      circles.add(
+          Circle(
+              "deviceLocation",
+              CircleOptions(
+                  geometry: latLng,
+                  circleRadius: 5,
+                  circleColor: '#3366FF',
+                  circleStrokeWidth: 2,
+                  circleStrokeColor: '#FFFFFF'
+              )
+          )
+      );
+    } else {
+      _mapController.addCircle(
+          CircleOptions(
+              geometry: latLng,
+              circleRadius: 5,
+              circleColor: '#3366FF',
+              circleStrokeWidth: 2,
+              circleStrokeColor: '#FFFFFF'
+          )
+      ).then((circle) => circles.add(circle));
+    }
   }
 
   @override
@@ -31,7 +83,7 @@ class _MapWidgetState extends State<MapWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return  MapboxMap(
+    return MapboxMap(
       accessToken: Keys.MapBoxKey,
       styleString: Keys.MapBoxNight,
       doubleClickZoomEnabled: false,
@@ -41,6 +93,9 @@ class _MapWidgetState extends State<MapWidget> {
       compassEnabled: false,
       onMapCreated: (controller) {
         _onMapCreated(controller);
+      },
+      onStyleLoadedCallback: () {
+        _getCurrentLocation();
       },
       initialCameraPosition: CameraPosition(
         target: Keys.MapCenter,
