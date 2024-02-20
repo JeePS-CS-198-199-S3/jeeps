1cf43d39e63a0e78840576e8bc5b97f0676fd08b
SmurfNiZed <zipacaldo@up.edu.ph>
2024-02-20 16:02:24 +0800

    feature: location of device is updating

--- transitrack_web/lib/components/map_related/map.dart
+++ transitrack_web/lib/components/map_related/map.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 
 import '../../config/keys.dart';
+import '../../config/map_settings.dart';
 
 class MapWidget extends StatefulWidget {
   final bool isHover;
@@ -14,59 +17,39 @@ class MapWidget extends StatefulWidget {
 
 class _MapWidgetState extends State<MapWidget> {
   late MapboxMapController _mapController;
+  late StreamSubscription  _positionStream;
   List<Circle> circles = [];
+  bool exists = false;
 
   void _onMapCreated(MapboxMapController controller) {
     _mapController = controller;
   }
 
-  Future<void> _getCurrentLocation() async {
-    await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.bestForNavigation)
-        .then(
-          (position) => {
-            _updateCircle(LatLng(position.latitude, position.longitude))
-          }
-        );
+  void _listenToLocation() {
+    _positionStream = Geolocator.getPositionStream(
+      locationSettings: const LocationSettings(
+        accuracy: LocationAccuracy.bestForNavigation,
+      ),
+    ).listen((Position position) {
+      _updateCircle(LatLng(position.latitude, position.longitude));
+    });
   }
 
   void _updateCircle(LatLng latLng) {
-
-    bool exists = false;
-
-    for (var circle in circles) {
-      if (circle.options.circleColor == '#3366FF') {
-        exists = true;
-        break;
-      }
-    }
-
     if (exists) {
-      Circle deviceCircle = circles.where((circle) => circle.options.circleColor == '#3366FF').first;
+      Circle deviceCircle = circles.where((circle) => circle.options.circleColor == deviceCircleColor).first;
       _mapController.updateCircle(deviceCircle, CircleOptions(geometry: latLng));
-      circles.removeWhere((circle) => circle.options.circleColor == '#3366FF');
-      circles.add(
-          Circle(
-              "deviceLocation",
-              CircleOptions(
-                  geometry: latLng,
-                  circleRadius: 5,
-                  circleColor: '#3366FF',
-                  circleStrokeWidth: 2,
-                  circleStrokeColor: '#FFFFFF'
-              )
-          )
-      );
     } else {
       _mapController.addCircle(
           CircleOptions(
               geometry: latLng,
               circleRadius: 5,
-              circleColor: '#3366FF',
+              circleColor: deviceCircleColor,
               circleStrokeWidth: 2,
               circleStrokeColor: '#FFFFFF'
           )
       ).then((circle) => circles.add(circle));
+      exists = true;
     }
   }
 
@@ -78,6 +61,7 @@ class _MapWidgetState extends State<MapWidget> {
   @override
   void dispose() {
     _mapController.dispose();
+    _positionStream.cancel();
     super.dispose();
   }
 
@@ -87,19 +71,20 @@ class _MapWidgetState extends State<MapWidget> {
       accessToken: Keys.MapBoxKey,
       styleString: Keys.MapBoxNight,
       doubleClickZoomEnabled: false,
-      minMaxZoomPreference: const MinMaxZoomPreference(14, 19),
+      minMaxZoomPreference: MinMaxZoomPreference(mapMinZoom, mapMaxZoom),
       scrollGesturesEnabled: !widget.isHover,
       tiltGesturesEnabled: false,
       compassEnabled: false,
+      rotateGesturesEnabled: false,
       onMapCreated: (controller) {
         _onMapCreated(controller);
       },
       onStyleLoadedCallback: () {
-        _getCurrentLocation();
+        _listenToLocation();
       },
       initialCameraPosition: CameraPosition(
         target: Keys.MapCenter,
-        zoom: 15,
+        zoom: mapStartZoom,
       ),
     );
   }
---
+++ transitrack_web/lib/config/map_settings.dart
@@ -0,0 +1,5 @@
+double mapStartZoom = 15;
+double mapMinZoom = 14;
+double mapMaxZoom = 19;
+
+String deviceCircleColor = '#3366FF';
